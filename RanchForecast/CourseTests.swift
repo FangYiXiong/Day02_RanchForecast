5a3e80c658b39fe1da937b8a812f07eada08ac49
方一雄 <fang@fangdeMacBook-Pro.local>
2015-05-12 10:03:47 -0700

    the first test

---
+++ RanchForecast/CourseTests.swift
@@ -0,0 +1,15 @@
+//
+//  CourseTests.swift
+//  RanchForecast
+//
+//  Created by fang on 15/5/12.
+//  Copyright (c) 2015年 Big Nerd Ranch. All rights reserved.
+//
+import XCTest
+
+class Course: XCTestCase {
+    func testCourseInitialization() {
+    
+    }
+}
+
